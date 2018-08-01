82c12fe578cda4d4264c25f274a114b36a86012f
CelloRen <939549708@qq.com>
2018-08-01 20:14:27 +0800

    Just read the code and draw a picture

---
+++ 206ReverseLinkedList.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev = null;    
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }       
+        return prev;    
+    }
+}
